9fa41791c3678f81f6cde2403fad942ffa43ab49
kandaharu <yuugengaisha@gmail.com>
2018-03-18 23:18:21 +0900

    fix bootstrap css-styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Fizz-Buzz Generator';
+  title = 'FizzBuzz Generator';
 }
