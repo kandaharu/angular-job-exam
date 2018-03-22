69d3eeac65b7f1ca357689ec316a052fc209f0f5
kandaharu <yuugengaisha@gmail.com>
2018-03-22 21:55:06 +0900

    fix 'CounterService#next' spec

--- src/app/counter.service.spec.ts
+++ src/app/counter.service.spec.ts
@@ -2,14 +2,48 @@ import { TestBed, inject } from '@angular/core/testing';
 
 import { CounterService } from './counter.service';
 
-describe('CounterService', () => {
+describe ('CounterService', () => {
+  let service: CounterService;
+
   beforeEach(() => {
-    TestBed.configureTestingModule({
-      providers: [CounterService]
-    });
+    TestBed.configureTestingModule({ providers: [CounterService] });
+    service = new CounterService();
   });
 
-  it('should be created', inject([CounterService], (service: CounterService) => {
+  it ('should be created', inject([CounterService], () => {
     expect(service).toBeTruthy();
   }));
+
+  describe ('#next', () => {
+    let prev: number;
+
+    describe ('execute the method once', () => {
+      beforeEach(() => { service.next(); });
+      it ('current should set 0', () => {
+        expect(service.current).toBe(0);
+      });
+    });
+
+    describe ('execute the method several times', () => {
+      beforeEach(() => {
+        for (let i = 0; i < 3; i++) { service.next(); }
+        prev = service.current;
+        service.next();
+      });
+      it ('current should set +1 value', () => {
+        expect(service.current).toBe(prev + 1);
+      });
+    });
+
+    describe ('execute the method over "to" value times', () => {
+      beforeEach(() => {
+        service.current = service.to;
+        prev = service.current;
+        service.next();
+      });
+      it ('current should set "to" value', () => {
+        expect(service.current).toBe(prev);
+      });
+    });
+  });
 });
