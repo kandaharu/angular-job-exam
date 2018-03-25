be8691f04f043662a65b1bd585cd78f0069c9777
kandaharu <yuugengaisha@gmail.com>
2018-03-25 21:38:40 +0900

    fix 'AppComponent#*' spec

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,27 +1,43 @@
 import { TestBed, async } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
+
 import { AppComponent } from './app.component';
-describe('AppComponent', () => {
+import { FizzbuzzComponent } from './fizzbuzz/fizzbuzz.component';
+import { ResultComponent } from './result/result.component';
+import { ResultService } from './result.service';
+import { CounterService } from './counter.service';
+
+describe ('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [ FormsModule ],
       declarations: [
-        AppComponent
+        AppComponent,
+        FizzbuzzComponent,
+        ResultComponent
       ],
+      providers: [
+        { provide: ResultService, useClass: ResultService },
+        { provide: CounterService, useClass: CounterService },
+      ]
     }).compileComponents();
   }));
-  it('should create the app', async(() => {
+  it ('should create the app', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'app'`, async(() => {
+
+  it (`should have as title 'FizzBuzz Generator'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
+    expect(app.title).toEqual('FizzBuzz Generator');
   }));
-  it('should render title in a h1 tag', async(() => {
+
+  it ('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
+    expect(compiled.querySelector('h1').textContent).toContain('FizzBuzz Generator');
   }));
 });
